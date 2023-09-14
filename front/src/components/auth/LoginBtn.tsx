371ae2d40d60746f3f4dc09346be25b464c9b625
uablejyh <jyh2610@uable.co.kr>
2023-09-14 14:27:11 +0900

    Feature: 소셜 로그인 컴포넌트화

--- front/src/components/auth/LoginBtn.tsx
+++ front/src/components/auth/LoginBtn.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { signIn, signOut } from "next-auth/react";
+import { signIn } from "next-auth/react";
 
 import { loginObj } from "@/constants/constants";
 
@@ -9,19 +9,14 @@ function LoginBtn() {
     <>
       {loginObj.map((data) => {
         return (
-          <div key={data.social}>
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              key={data.social}
-              type="button"
-              onClick={() => signIn(data.social)}
-            >
-              {data.name}
-            </button>
-            <button type="button" onClick={() => signOut()}>
-              logout
-            </button>
-          </div>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            key={data.social}
+            type="button"
+            onClick={() => signIn(data.social)}
+          >
+            {data.name}
+          </button>
         );
       })}
     </>
