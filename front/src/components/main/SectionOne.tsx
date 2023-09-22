27c3ba9f5e42f7d45e4d1ee68d6943a37a65eb5d
jyh2610 <jogg4177@naver.com>
2023-09-22 18:27:44 +0900

    ADD: redux

--- front/src/components/main/SectionOne.tsx
+++ front/src/components/main/SectionOne.tsx
@@ -4,11 +4,11 @@ import { motion } from "framer-motion";
 
 const SectionOne = () => {
   const [scrollY, setScrollY] = useState(0);
-
+  const [scrollDisabled, setScrollDisabled] = useState(true);
   // 스크롤 이벤트 리스너를 추가하여 스크롤 위치를 감지합니다.
   useEffect(() => {
     const handleScroll = () => {
-      setScrollY(window.scrollY);
+      scrollDisabled && setScrollY(window.scrollY);
     };
 
     window.addEventListener("scroll", handleScroll);
@@ -21,7 +21,7 @@ const SectionOne = () => {
   // 스크롤 위치에 따라 애니메이션을 제어합니다.
   const animationVariants = {
     initial: { scale: 1.5 }, // 초기 스케일 설정 (작은 크기)
-    scrolled: { scale: 4 }, // 스크롤 시 크게 확대
+    scrolled: { scale: 6 }, // 스크롤 시 크게 확대
   };
 
   const animationTransition = {
@@ -29,7 +29,7 @@ const SectionOne = () => {
   };
 
   return (
-    <section className="h-screen flex justify-center items-center">
+    <section className="h-screen flex justify-center items-center relative">
       {/* section 스타일을 설정하여 섹션의 높이와 정렬을 지정 */}
       <motion.div
         initial="initial"
@@ -37,13 +37,13 @@ const SectionOne = () => {
         variants={animationVariants}
         transition={animationTransition}
         style={{
-          width: "100px",
+          width: "50%",
           height: "100px",
+          position: "absolute",
+          bottom: "0",
           backgroundColor: "blue",
         }}
-      >
-        Scroll Me!
-      </motion.div>
+      ></motion.div>
     </section>
   );
 };
