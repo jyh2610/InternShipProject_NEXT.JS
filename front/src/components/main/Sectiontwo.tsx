f2c4eb25629411dd6103e666531b80397919dd1a
jyh2610 <jogg4177@naver.com>
2023-09-25 23:20:11 +0900

    fix:width 레이아웃 수정

--- front/src/components/main/Sectiontwo.tsx
+++ front/src/components/main/Sectiontwo.tsx
@@ -1,13 +1,15 @@
 import SectionProvider from "./SectionProvider";
+import BotText from "./section-2/BotText";
 import Text from "./section-2/Text";
 import TwoCaruosel from "./section-2/TwoCaruosel";
 
 function Sectiontwo() {
   const sliderHander = () => {};
   return (
-    <section className="h-screen ">
+    <section className="h-screen">
       <Text />
       <TwoCaruosel />
+      <BotText />
     </section>
   );
 }
--- front/src/components/main/section-2/TwoCaruosel.tsx
+++ front/src/components/main/section-2/TwoCaruosel.tsx
@@ -15,35 +15,22 @@ function TwoCarousel() {
   const [imgIdx, setImgIdx] = useState(0);
   const dummyData = [1, 2, 3, 4];
   const handleSlideChange = (swiper: { realIndex: number }) => {
-    // setImgIdx(idx);
     setImgIdx(swiper.realIndex);
   };
 
   return (
-    <>
-      <div className="w-4/5 flex justify-start">
-        <Swiper
-          className="mySwiper "
-          cssMode={true}
-          navigation={true}
-          pagination={true}
-          mousewheel={true}
-          keyboard={true}
-          loop={true}
-          modules={[Navigation, Pagination]}
-          onSlideChange={handleSlideChange}
-        >
-          {dummyData.map((_, idx) => (
-            <SwiperSlide key={idx}>
-              <Image src={dummyImg} alt={"dummy"} width={1800} height={600} />
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-      <span>
+    <div className="flex justify-start">
+      <Swiper className="mySwiper w-4/5" navigation={true} loop={true} modules={[Navigation, Pagination]} onSlideChange={handleSlideChange}>
+        {dummyData.map((_, idx) => (
+          <SwiperSlide key={idx}>
+            <Image src={dummyImg} alt={"dummy"} width={1200} height={600} layout="responsive" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <span className="mt-auto mb-0">
         {imgIdx + 1} / {dummyData.length}
       </span>
-    </>
+    </div>
   );
 }
 
