d6aeae7baa46ca8d75aa61d460dae402c76d964f
jyh2610 <jogg4177@naver.com>
2023-09-25 10:32:41 +0900

    ADD: redux

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -6,12 +6,12 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <SectionOne />
       <SectionFour />
       <SectionFive />
       <SectionSix />
       <Footer />
-    </div>
+    </>
   );
 }
--- front/src/components/nav/Nav.tsx
+++ front/src/components/nav/Nav.tsx
@@ -1,18 +1,34 @@
 "use client";
-import { Menu } from "antd";
 
+import { Layout } from "antd";
 import { navItems } from "@/constants/navConst";
+import { useAppDispatch } from "@/redux/hooks";
+import { toggleIsNav } from "@/redux/slicer/scrollStopper";
+import { useRef } from "react";
+
+const { Header } = Layout;
 
 function Nav() {
+  const ref = useRef(null);
   const items = navItems.map((item, idx) => {
     const key = idx + 1;
     return { key, label: item };
   });
-
+  const dispatch = useAppDispatch();
+  console.log(ref.current);
   return (
-    <Menu style={{ width: "100%", backgroundColor: "#F8F8FA", padding: "1px", position: "fixed" }} mode="horizontal">
+    <Header
+      ref={ref}
+      style={{
+        width: "100%",
+        height: "100px",
+        backgroundColor: "#F8F8FA",
+        padding: "1px",
+        position: "fixed",
+      }}
+    >
       <div className="border-2 border-indigo-600 h-16">1111212312</div>
-    </Menu>
+    </Header>
   );
 }
 
