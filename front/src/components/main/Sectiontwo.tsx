0f527fafacd35396e7d95dd826a1c7f24369f792
jyh2610 <jogg4177@naver.com>
2023-09-25 17:27:55 +0900

    feat : sectionTwo slider 인덱스 추적 렌더링

--- front/src/components/main/Sectiontwo.tsx
+++ front/src/components/main/Sectiontwo.tsx
@@ -5,7 +5,7 @@ import TwoCaruosel from "./section-2/TwoCaruosel";
 function Sectiontwo() {
   const sliderHander = () => {};
   return (
-    <section className="w-3/5 h-screen">
+    <section className="h-screen">
       <Text />
       <TwoCaruosel />
     </section>
--- front/src/components/main/section-2/TwoCaruosel.tsx
+++ front/src/components/main/section-2/TwoCaruosel.tsx
@@ -2,38 +2,48 @@
 
 import Image from "next/image";
 import { dummyImg } from "@/constants/constants";
-
 import { Swiper, SwiperSlide } from "swiper/react";
-
 import { Navigation, Pagination } from "swiper/modules";
+
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+import { useRef, useState } from "react";
 
-function TwoCaruosel() {
+function TwoCarousel() {
+  const ref = useRef(null);
+  const [imgIdx, setImgIdx] = useState(0);
   const dummyData = [1, 2, 3, 4];
+  const handleSlideChange = (swiper: { realIndex: any }) => {
+    // setImgIdx(idx);
+    setImgIdx(swiper.realIndex);
+  };
+  console.log(imgIdx);
 
   return (
-    <Swiper
-      slidesPerView={1}
-      spaceBetween={30}
-      loop={true}
-      pagination={{
-        clickable: true,
-      }}
-      navigation={true}
-      modules={[Pagination, Navigation]}
-      className="mySwiper"
-    >
-      {dummyData.map((_, idx) => {
-        return (
+    <>
+      <Swiper
+        className="mySwiper w-3/5"
+        cssMode={true}
+        navigation={true}
+        pagination={true}
+        mousewheel={true}
+        keyboard={true}
+        loop={true}
+        modules={[Navigation, Pagination]}
+        onSlideChange={handleSlideChange}
+      >
+        {dummyData.map((_, idx) => (
           <SwiperSlide key={idx}>
-            <Image src={dummyImg} alt={"dummy"} width={600} height={400} />
+            <Image src={dummyImg} alt={"dummy"} width={300} height={300} layout="responsive" />
           </SwiperSlide>
-        );
-      })}
-    </Swiper>
+        ))}
+      </Swiper>
+      <span>
+        {imgIdx + 1} / {dummyData.length}
+      </span>
+    </>
   );
 }
 
-export default TwoCaruosel;
+export default TwoCarousel;
