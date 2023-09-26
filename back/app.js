01b69b73ca4e4f63bf64f5948a9ea5b5528a2a25
RaedeokHwang <141380757+RaedeokHwang@users.noreply.github.com>
2023-09-26 10:59:42 +0900

    Feat: morgan, cors 추가

--- back/app.js
+++ back/app.js
@@ -1,19 +1,15 @@
 "use strict";
 
 const express = require('express');
-const dotenv = require('dotenv');
-const all_db = require('./src/db/all');
 const routes = require('./src/routes'); // index.js는 생략 가능
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
-dotenv.config();
 
-all_db.member.query('SELECT * FROM user', (err, results) => {
-  if (err) {
-    console.error('Error executing query:', err);
-    return;
-  }
-  console.log('Query results:', results);
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(morgan('dev'));
 
 module.exports = app;
