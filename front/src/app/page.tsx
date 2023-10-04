9f122442ebcc606397750e10cba212305cc433c7
uablejyh <jyh2610@uable.co.kr>
2023-10-04 09:51:19 +0900

    feat: 상단바 접촉 이벤트

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import dynamic from "next/dynamic";
+
 import SectionOne from "@/components/main/SectionOne";
 import SectionFour from "@/components/main/SectionFour";
 import SectionFive from "@/components/main/SectionFive";
@@ -8,7 +8,7 @@ import Footer from "@/components/Footer";
 import Sectiontwo from "@/components/main/Sectiontwo";
 import FadeProvider from "@/components/FadeProvider";
 import SectionThree from "@/components/main/SectionThree";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
@@ -16,34 +16,16 @@ export default function Home() {
       window.scrollTo(0, 0);
     };
   }, []);
-  const parentRef = useRef<HTMLDivElement | null>(null);
-  const childRef = useRef<HTMLDivElement | null>(null);
-
-  const isClose = () => {
-    let chTop = childRef.current?.getBoundingClientRect().top;
-    let peTop = parentRef.current?.getBoundingClientRect().top;
-    console.log("부모요소와의 거리(각 요소의  TOP),", chTop! - peTop!);
-  };
-
-  useEffect(() => {
-    isClose();
-  }, []);
 
   return (
     <FadeProvider>
-      <>
-        <div ref={parentRef}>
-          <SectionOne />
-        </div>
-        <div ref={childRef}>
-          <Sectiontwo />
-        </div>
-        <SectionThree />
-        <SectionFour />
-        <SectionFive />
-        <SectionSix />
-        {/* <Footer /> */}
-      </>
+      <SectionOne />
+      <Sectiontwo />
+      <SectionThree />
+      <SectionFour />
+      <SectionFive />
+      <SectionSix />
+      {/* <Footer /> */}
     </FadeProvider>
   );
 }
--- front/src/components/main/SectionOne.tsx
+++ front/src/components/main/SectionOne.tsx
@@ -1,20 +1,33 @@
 "use client";
-import { motion, useAnimation, useScroll } from "framer-motion";
-import { useEffect, useRef } from "react";
-import SectionProvider from "./SectionProvider";
+import React, { useEffect, useRef } from "react";
+import { motion, useAnimation } from "framer-motion";
 import { dummyImg } from "@/constants/constants";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { sectionContact } from "@/redux/slicer/scrollStopper";
+import SectionProvider from "./SectionProvider";
 
 const SectionOne = () => {
   const controls = useAnimation();
   const sectionOneRef = useRef<HTMLImageElement | null>(null);
   const dispatch = useAppDispatch();
 
+  const rect = useAppSelector((state) => state.scrollStopper);
+
+  const navBot = rect.navBottom;
+  const sectionTop = rect.sectionTop;
+  const isContact = navBot - sectionTop;
+  console.log(isContact, sectionTop);
+
   useEffect(() => {
     const handleScroll = () => {
       const sectionOneTop = sectionOneRef.current?.getBoundingClientRect().top;
       dispatch(sectionContact({ section: sectionOneTop }));
+
+      // Check if isContact is greater than or equal to 0
+      if (isContact >= 0) {
+        // If isContact is greater than or equal to 0, expand the image to full screen
+        controls.start({ width: "100vw", height: "70vh" });
+      }
     };
 
     window.addEventListener("scroll", handleScroll, { passive: true });
@@ -22,22 +35,7 @@ const SectionOne = () => {
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
-  }, [dispatch]);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      const maxScrollY = document.documentElement.scrollHeight - window.innerHeight;
-
-      controls.start({ width: "100vw", height: `${90}vh` });
-    };
-
-    window.addEventListener("scroll", handleScroll, { passive: true });
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [controls]);
+  }, [dispatch, isContact, controls]);
 
   return (
     <SectionProvider>
