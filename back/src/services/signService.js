9f4678219b4e4b707415f98babc41b4a198cff60
Uable-hrd <hrd1@uable.co.kr>
2023-10-06 18:28:00 +0900

    Feat: utils/validateToken 추가

--- back/src/services/signService.js
+++ back/src/services/signService.js
@@ -4,16 +4,29 @@ const auth = require("../models/auth");
 const member = require("../models/member");
 
 //local SignUp
-const localSignUp = async (user) => {
-
-};
+const localSignUp = async (user) => {};
 
 //local SignIn
-const localSignIn = async (user) => {
+const localSignIn = async (user) => {};
 
-};
+
+// const signIn = async (email, password) => {
+//   const hashedPassword = await userDao.getHashedPassword(email);
+//   if (!hashedPassword) detectError("PASSWORD_DOES_NOT_MATCH", 400);
+
+//   const compare = await bcrypt.compare(password, hashedPassword);
+//   if (!compare) detectError("PASSWORD_DOES_NOT_MATCH", 400);
+
+//   const [userData] = await userDao.getUserId(email);
+
+//   const payLoad = { userId: userData.id };
+
+//   const jwtToken = jwt.sign(payLoad, process.env.JWT_SECRET);
+
+//   return jwtToken;
+// };
 
 module.exports = {
-    localSignUp,
-    localSignIn
+  localSignUp,
+  localSignIn,
 };
