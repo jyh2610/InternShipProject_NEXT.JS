b2dc8d16553fd16db2b3f9f9e9a5fa4e4062f776
DevHyunJ <sonhyunji9819@gmail.com>
2023-10-06 11:58:34 +0900

    Fix: 반응형 작업 진행중

--- front/src/components/main/SectionFour.tsx
+++ front/src/components/main/SectionFour.tsx
@@ -11,14 +11,11 @@ function SectionFour() {
   const isContact = scrollTopHeight - scrollHeight > 0;
   return (
     <>
-      <SectionProvider>
-        <div ref={sectionTopRef} data-anchor="Page 2" className="text-7xl my-[10%] h-[50vh]" style={{ overflow: "hidden" }}>
-          <ParallaxText baseVelocity={-4}>공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</ParallaxText>
-          <ParallaxText baseVelocity={3}>인테리어는 더 나은 삶을 위한 시작입니다. 우리와 함께 새로운 시작을 경험하세요.</ParallaxText>
-          <ParallaxText baseVelocity={-4}>인테리어는 더 나은 삶을 위한 시작입니다. 우리와 함께 새로운 시작을 경험하세요.</ParallaxText>
-        </div>
-      </SectionProvider>
-      )
+      <div ref={sectionTopRef} data-anchor="Page 2" className="text-7xl bg-[#fff] h-[50vh]" style={{ overflow: "hidden" }}>
+        <ParallaxText baseVelocity={-4}>공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</ParallaxText>
+        <ParallaxText baseVelocity={3}>인테리어는 더 나은 삶을 위한 시작입니다. 우리와 함께 새로운 시작을 경험하세요.</ParallaxText>
+        <ParallaxText baseVelocity={-4}>인테리어는 더 나은 삶을 위한 시작입니다. 우리와 함께 새로운 시작을 경험하세요.</ParallaxText>
+      </div>
     </>
   );
 }
