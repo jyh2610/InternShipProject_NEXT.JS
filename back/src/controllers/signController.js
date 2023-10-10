f9bce5c17a34835386989461680752f7db5581b8
Uable-hrd <hrd1@uable.co.kr>
2023-10-10 13:31:14 +0900

    Feat: SignController의 로컬 로그인, 회원가입 구현

--- back/src/controllers/signController.js
+++ back/src/controllers/signController.js
@@ -6,4 +6,26 @@ const {catchAsync, detectError} = require("../utils/detectError");
 // local signUP
 const signUp =  catchAsync(async(req, res) => {
     const {nickname, user_name, email, password, birthday, nation, sex} = req.body;
+
+    if(!user_name || !email || !password) detectError("KEY_ERROR", 400);
+
+    await userService.signUp(nickname, user_name, email, password, birthday, nation, sex);
+
+    return res.status(201).json({message: "USER_CREATED", success: true});
 });
+
+// local signIn
+const signIn = catchAsync(async(req, res) => {
+    const {user_name, password} = req.body;
+
+    if (!user_name || !password) detectError("KEY_ERROR", 400);
+
+    token = await userService.signIn(user_name, password);
+
+    return res.status(200).json({accessToken: token, success: true});
+});
+
+module.exports ={
+    signUp,
+    signIn
+};
