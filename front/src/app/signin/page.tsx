f4032cbc58fbe5338d103b4561f7d5de078bd197
DevHyunJ <sonhyunji9819@gmail.com>
2023-10-10 18:02:37 +0900

    Fix: 로그인 페이지 폼 적용

--- front/src/app/signin/page.tsx
+++ front/src/app/signin/page.tsx
@@ -1,37 +1,62 @@
-import LoginBtn from "@/components/auth/LoginBtn";
+"use client";
 import React from "react";
 import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { Button, Checkbox, Form, Input } from "antd";
-function SignIn() {
+import SigninTitle from "@/components/signin/SigninTitle";
+const SignIn = () => {
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
   return (
-    <>
-      <Form name="normal_login" className="login-form" initialValues={{ remember: true }}>
-        <Form.Item name="username" rules={[{ required: true, message: "Please input your Username!" }]}>
+    <div className="pt-[11rem]">
+      <SigninTitle />
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "아이디 입력",
+            },
+          ]}
+        >
           <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
         </Form.Item>
-        <Form.Item name="password" rules={[{ required: true, message: "Please input your Password!" }]}>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "비밀번호 입력",
+            },
+          ]}
+        >
           <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password" />
         </Form.Item>
         <Form.Item>
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
-
-          <a className="login-form-forgot" href="">
-            Forgot password
-          </a>
-        </Form.Item>
-
-        <Form.Item>
           <Button type="primary" htmlType="submit" className="login-form-button">
-            Log in
+            로그인
           </Button>
-          Or <a href="">register now!</a>
+        </Form.Item>
+        <Form.Item>
+          <a className="login-form-forgot" href="">
+            아이디 찾기
+          </a>
+          <a className="login-form-forgot" href="">
+            비밀번호 찾기
+          </a>
+          <a href="">회원가입</a>
         </Form.Item>
       </Form>
-      <LoginBtn />
-    </>
+    </div>
   );
-}
+};
 
 export default SignIn;
---
+++ front/src/components/signin/SigninTitle.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SigninTitle = () => {
+  return <div>SigninTitle</div>;
+};
+
+export default SigninTitle;
