dbc6f7ad17757810b6545579f6169f813a4507d7
DevHyunJ <sonhyunji9819@gmail.com>
2023-10-11 14:51:31 +0900

    Fix: 섹션 4 인터랙션 작업 진행중

--- front/src/components/main/SectionFive.tsx
+++ front/src/components/main/SectionFive.tsx
@@ -1,27 +1,11 @@
 "use client";
 import React from "react";
 import "../../components/main/section-5/style.css";
+import HoverImage from "./section-5/HoverImage";
 function SectionFive() {
   return (
-    <div className="flex">
-      <div className="relative w-1/4 h-40 bg-gray-200 overflow-hidden transition-all duration-300 group hover:w-[100vw]">
-        <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 group-hover:opacity-100">
-          <img src="https://user-images.githubusercontent.com/144188723/273080155-924670d8-ffc1-433e-9a39-80ba6c1edd47.jpg" alt="이미지" />{" "}
-          <p className="text-gray-700 font-semibold">호버 텍스트</p>
-        </div>
-      </div>
-      <div className="relative w-1/4 h-40 bg-gray-200 overflow-hidden transition-all duration-300 group hover:w-[100vw]">
-        <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 group-hover:opacity-100">
-          <img src="https://user-images.githubusercontent.com/144188723/273080155-924670d8-ffc1-433e-9a39-80ba6c1edd47.jpg" alt="이미지" />{" "}
-          <p className="text-gray-700 font-semibold">호버 텍스트</p>
-        </div>
-      </div>
-      <div className="relative w-1/4 h-40 bg-gray-200 overflow-hidden transition-all duration-300 group hover:w-[100vw]">
-        <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 group-hover:opacity-100">
-          <img src="https://user-images.githubusercontent.com/144188723/273080155-924670d8-ffc1-433e-9a39-80ba6c1edd47.jpg" alt="이미지" />{" "}
-          <p className="text-gray-700 font-semibold">호버 텍스트</p>
-        </div>
-      </div>
+    <div>
+      <HoverImage />
     </div>
   );
 }
--- front/src/components/main/section-5/HoverImage.tsx
+++ front/src/components/main/section-5/HoverImage.tsx
@@ -1,14 +1,18 @@
+import { data } from "@/constants/constants";
 import React from "react";
 
 const HoverImage = () => {
   return (
-    <div>
-      <div className="relative w-1/4 h-40 bg-gray-200 overflow-hidden transition-all duration-300 group hover:w-[100vw]">
-        <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 group-hover:opacity-100">
-          <img src="https://user-images.githubusercontent.com/144188723/273080155-924670d8-ffc1-433e-9a39-80ba6c1edd47.jpg" alt="이미지" />{" "}
-          <p className="text-gray-700 font-semibold">호버 텍스트</p>
+    <div className="flex">
+      {data.map((item, idx) => (
+        <div key={idx} className="relative w-1/4 h-[56.25rem] overflow-hidden transition-all duration-300 group hover:w-[100vw]">
+          <img src={item.img} alt="이미지" />{" "}
+          <div className="absolute inset-0 flex items-center justify-center transition-opacity duration-300 ">
+            <p className="text-[4rem] text-[#fff] ">0{item.id + 1}</p>
+            <p className="opacity-0 hover:opacity-100 text-gray-700 font-semibold">{item.contentstext}</p>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
