4dec387b1cb610ce09b47bcb58437b12c5d0f06d
DevHyunJ <sonhyunji9819@gmail.com>
2023-10-12 17:08:43 +0900

    Fix:네브에서 로그인 페이지 이동

--- front/src/components/nav/NavRight.tsx
+++ front/src/components/nav/NavRight.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import NavDropDown from "./NavDropDown";
 import { Button, MenuProps } from "antd";
+import { useRouter } from "next/navigation";
 
 function NavRight() {
+  const route = useRouter();
+  const moveSignin = () => route.push("/signin");
   const data: MenuProps = {
     items: [
       { key: "1", label: <a>한국어</a> },
@@ -13,7 +16,7 @@ function NavRight() {
   return (
     <div>
       <NavDropDown title={"한국어"} items={data} />
-      <Button style={{ borderRadius: "14px", color: "white", fontSize: "0.75rem" }} type="text">
+      <Button onClick={moveSignin} style={{ borderRadius: "14px", color: "white", fontSize: "0.75rem" }} type="text">
         로그인
       </Button>
     </div>
