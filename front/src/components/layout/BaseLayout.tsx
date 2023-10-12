5aa9eef4ee17e0e0b0b77d4f50458137e29fc0c3
DevHyunJ <sonhyunji9819@gmail.com>
2023-10-12 11:36:00 +0900

    Fix:로그인 버튼 수정

--- front/src/components/layout/BaseLayout.tsx
+++ front/src/components/layout/BaseLayout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-
-const BaseLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+import { ReactNode } from "react";
+const BaseLayout = (props: { children: ReactNode }) => {
+  return <div> {props.children}</div>;
 };
 
 export default BaseLayout;
