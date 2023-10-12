9cd0888bebcbd524c78a8523ed28543bf6f65c77
uablejyh <jyh2610@uable.co.kr>
2023-10-12 13:55:36 +0900

    feat:인덱스 슬라이드 버튼간 라인

--- front/src/components/main/section-4/TextAnimation.tsx
+++ front/src/components/main/section-4/TextAnimation.tsx
@@ -11,24 +11,24 @@ function TextAnimation() {
     // </div>
     <div className="flow-container">
       <div className="flow-text">
-          <div className="flow-wrap flow-wrap-left">공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</div>
-          <div className="flow-wrap flow-wrap-left">공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</div>
-          <div className="flow-wrap flow-wrap-left">공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</div>
-          <div className="flow-wrap flow-wrap-left">공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</div>
+        <div className="flow-wrap flow-wrap-left">공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</div>
+        <div className="flow-wrap flow-wrap-left">공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</div>
+        <div className="flow-wrap flow-wrap-left">공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</div>
+        <div className="flow-wrap flow-wrap-left">공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</div>
       </div>
       <div className="flow-text">
-          <div className="flow-wrap-right">인테리어는 더 나은 삶을 위한 시작입니다. 우리와 함께 새로운 시작을 경험하세요.</div>
-          <div className="flow-wrap-right">인테리어는 더 나은 삶을 위한 시작입니다. 우리와 함께 새로운 시작을 경험하세요.</div>
-          <div className="flow-wrap-right">인테리어는 더 나은 삶을 위한 시작입니다. 우리와 함께 새로운 시작을 경험하세요.</div>
-          <div className="flow-wrap-right">인테리어는 더 나은 삶을 위한 시작입니다. 우리와 함께 새로운 시작을 경험하세요.</div>
+        <div className="flow-wrap-right">인테리어는 더 나은 삶을 위한 시작입니다. 우리와 함께 새로운 시작을 경험하세요.</div>
+        <div className="flow-wrap-right">인테리어는 더 나은 삶을 위한 시작입니다. 우리와 함께 새로운 시작을 경험하세요.</div>
+        <div className="flow-wrap-right">인테리어는 더 나은 삶을 위한 시작입니다. 우리와 함께 새로운 시작을 경험하세요.</div>
+        <div className="flow-wrap-right">인테리어는 더 나은 삶을 위한 시작입니다. 우리와 함께 새로운 시작을 경험하세요.</div>
       </div>
       <div className="flow-text">
-          <div className="flow-wrap-left">공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</div>
-          <div className="flow-wrap-left">공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</div>
-          <div className="flow-wrap-left">공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</div>
-          <div className="flow-wrap-left">공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</div>
+        <div className="flow-wrap-left">공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</div>
+        <div className="flow-wrap-left">공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</div>
+        <div className="flow-wrap-left">공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</div>
+        <div className="flow-wrap-left">공간은 이야기를 품고 있습니다. 우리는 그 이야기를 아름답게 만듭니다.</div>
       </div>
-  </div>
+    </div>
   );
 }
 export default TextAnimation;
