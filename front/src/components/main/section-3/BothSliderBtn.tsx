c2111631ded392048ac067341bcca7f7972664ab
uablejyh <jyh2610@uable.co.kr>
2023-10-13 14:25:39 +0900

    feat : 캐러셀 에러 수정

--- front/src/components/main/section-3/BothSliderBtn.tsx
+++ front/src/components/main/section-3/BothSliderBtn.tsx
@@ -1,13 +1,27 @@
+import React, { useEffect, useRef } from "react";
+
 import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
 
-function BothSliderBtn({ prevRef, nextRef }: { prevRef: any; nextRef: any }) {
+function BothSliderBtn({ setPrevRef, setNextRef }: { setPrevRef: Function; setNextRef: Function }) {
+  // useRef로 DOM 요소에 대한 레퍼런스 생성
+  const prevButtonRef = useRef(null);
+  const nextButtonRef = useRef(null);
+  console.log("btn", prevButtonRef);
+
+  useEffect(() => {
+    // 컴포넌트가 마운트되면 DOM 요소를 할당
+    setPrevRef(prevButtonRef.current);
+    setNextRef(nextButtonRef.current);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // 빈 배열을 사용하여 이펙트는 한 번만 실행
+
   return (
     <div className="flex justify-start gap-3 pl-10 mt-10">
-      <div ref={prevRef}>
-        <BsArrowLeft className="text-4xl  p-1 bg-bg-gray" />
+      <div ref={prevButtonRef}>
+        <BsArrowLeft className="text-4xl p-1 bg-bg-gray" />
       </div>
-      <div ref={nextRef}>
-        <BsArrowRight className="text-4xl p-1  bg-bg-gray" />
+      <div ref={nextButtonRef}>
+        <BsArrowRight className="text-4xl p-1 bg-bg-gray" />
       </div>
     </div>
   );
