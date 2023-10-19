3743a8915e4526017cdda423a08eaa3779833a1e
DevHyunJ <sonhyunji9819@gmail.com>
2023-10-19 14:27:36 +0900

    Fix: 소셜 로그인 디버깅 진행중

--- front/src/components/nav/NavRight.tsx
+++ front/src/components/nav/NavRight.tsx
@@ -7,63 +7,70 @@ import NavDropDown from "./NavDropDown";
 
 import type { MenuProps } from "antd";
 
-import { setAccessToken, setUserName } from "@/redux/slicer/authSlice";
+import { setAccessToken } from "@/redux/slicer/authSlice";
+
 import { signOut, useSession } from "next-auth/react";
+
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+
 import { baseApi } from "@/API/api";
+
 import { getCookie, removeCookie } from "@/API/cookie";
 
 function NavRight({ scrollY }: { scrollY: number }) {
   const dispatch = useAppDispatch();
   const route = useRouter();
+
   const moveSignin = () => route.push("/signin");
-  // 로컬 로그아웃
+  // 로컬 및 소셜 로그아웃
   const accesstoken = useAppSelector((state: any) => state.auth.accessToken);
   const api = new baseApi();
-
+  const { data: session } = useSession();
   // 마운트 될때 리프레시 토큰 보내기
-  useEffect(() => {
-    // 리프레시 토큰을 가져오는 코드
-    const refreshToken = getCookie("refresh_Token");
-
-    // 리프레시 토큰을 서버로 보내는 코드
-    const sendRefreshTokenToServer = async (refreshToken: string) => {
-      try {
-        const api = new baseApi();
-        const response = await api.withTokenPost(refreshToken, {
-          url: "/validate/reissuancetoken",
-          options: { headers: { Authorization: `Bearer ${refreshToken}` } },
-        });
-        const newAccessToken = response.accessToken;
-        setAccessToken(newAccessToken);
-        // 리덕스 스토어에도 저장
-        dispatch(setAccessToken(newAccessToken));
-      } catch (error) {
-        console.error("에러 발생:", error);
-        // 오류 처리
-      }
-    };
-
-    // 컴포넌트가 마운트될 때와 리렌더링될 때마다 실행
-    sendRefreshTokenToServer(refreshToken);
-  }, [dispatch]);
+  const refreshToken = getCookie("refresh_Token");
+  // 리프레시 토큰을 서버로 보내는 코드
+  const sendRefreshTokenToServer = async (refreshToken: string) => {
+    try {
+      const api = new baseApi();
+      const response = await api.post({
+        url: "/validate/reissuancetoken",
+        options: { headers: { Authorization: `Bearer ${refreshToken}` } },
+      });
+      const newAccessToken = response.accessToken;
+      // 리덕스 스토어에도 저장
+      dispatch(setAccessToken(newAccessToken));
+    } catch (error) {
+      console.error("에러 발생:", error);
+      // 오류 처리
+    }
+  };
   //로컬 로그아웃
   const logout = async () => {
+    await signOut({ callbackUrl: "/" });
+    await update(null);
     const url = "/sign/signout"; // 요청을 보낼 URL
-    const body = {};
     // Access Token 설정
     try {
-      const response = await api.withTokenPost(accesstoken, { url, body });
-      console.log("응답 데이터:_________________________", accesstoken, "\n", response);
+      const response = await api.post({
+        url,
+        options: {
+          headers: {
+            Authorization: `Bearer ${accesstoken}`, // yourAccessToken은 실제 엑세스 토큰 값으로 대체해야 합니다.
+          },
+        },
+      });
+      console.log("로그아웃응답 데이터:_________________________", response);
     } catch (error) {
       console.error("에러 발생:", error);
     }
     removeCookie("refresh_Token");
     dispatch(setAccessToken(null));
-
+    sendRefreshTokenToServer(refreshToken);
     route.push("/");
   };
-
+  useEffect(() => {
+    sendRefreshTokenToServer(refreshToken);
+  }, []);
   const data: MenuProps = {
     items: [
       {
@@ -82,7 +89,7 @@ function NavRight({ scrollY }: { scrollY: number }) {
   return (
     <div className="flex items-center">
       <NavDropDown scrollY={scrollY} title={"한국어"} items={data} />
-      {accesstoken ? (
+      {accesstoken || session ? (
         <Button onClick={logout} style={{ borderRadius: "14px", color: `${isTop}`, fontSize: "0.75rem" }} type="text">
           로그아웃
         </Button>
--- front/src/components/signin/SocialLoginButton.tsx
+++ front/src/components/signin/SocialLoginButton.tsx
@@ -1,43 +1,45 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import { useRouter } from "next/router";
+import NextAuth from "next-auth/next";
 import { signIn, useSession } from "next-auth/react";
 
 import { baseApi } from "@/API/api";
 import { setCookie } from "@/API/cookie";
 import { loginObj } from "@/constants/constants";
-import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { setAccessToken } from "@/redux/slicer/authSlice";
-import Link from "next/link";
 
 const SocialLoginButton = () => {
   const { data: session } = useSession();
-  const dispatch = useAppDispatch();
-  const router = useRouter;
+  useEffect(() => {
+    // `session` 콜백 함수를 사용하여 `session` 객체를 가져옵니다.
+    // const session = NextAuth?.session();
+    // `session` 객체에서 `token` 속성을 사용하여 토큰을 가져옵니다.
+    // const accessToken = session.token.accessToken;
+    // console.log(accessToken, "새로운 엑세스토큰 ?");
+    // ...
+  }, []);
 
   const api = new baseApi();
   // 소셜로그인
   const sociallogin = async (socialtype: string) => {
-    await signIn(socialtype, { callbackUrl: "/" });
-    const url = `/sign/${socialtype}login`;
-    // Access Token 설정
     try {
-      const socialaccesstoken: any | undefined = session?.accessToken || "";
+      await signIn(socialtype).then((res) => console.log(res));
+
+      // Access Token 설정
+      const socialaccesstoken = session?.accessToken;
+      const url = `/sign/${socialtype}login`;
       // api.setHeaders(socialaccesstoken);
       const response = await api.post({
         url,
         options: {
           headers: {
-            Authorization: `Bearer ${socialaccesstoken.accessToken}`, // yourAccessToken은 실제 엑세스 토큰 값으로 대체해야 합니다.
+            Authorization: `Bearer ${socialaccesstoken}`, // yourAccessToken은 실제 엑세스 토큰 값으로 대체해야 합니다.
           },
         },
       });
-      console.log(response, "________________");
       setCookie("refresh_Token", response.refreshToken);
-      dispatch(setAccessToken(response.accessToken));
-      return <Link href="/" />; // 홈페이지로 이동
     } catch (error) {
       console.error("에러 발생:", error);
       // next auth 에 accesstoken 을 지운다
