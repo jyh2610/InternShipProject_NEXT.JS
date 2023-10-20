0dd0ad950b27d8dbbc8f7f68740c10dc42cd6f3d
uablejyh <jyh2610@uable.co.kr>
2023-10-20 17:09:54 +0900

    fix: error

--- front/src/components/main/section-3/MySwiperComponent.tsx
+++ front/src/components/main/section-3/MySwiperComponent.tsx
@@ -1,38 +1,38 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-import Swiper from "swiper";
+// import Swiper from "swiper";
 import "./style.css";
 
 function MySwiperComponent() {
-  const [swiper, setSwiper] = useState(null);
+  // const [swiper, setSwiper] = useState(null);
 
-  useEffect(() => {
-    if (swiper === null) {
-      const newSwiper = new Swiper(".swiper-container", {
-        // Swiper 옵션 설정
-        slidesPerView: 2,
-        spaceBetween: 10,
-        loop: true,
-        loopedSlides: 1,
-        navigation: true,
-        // 다른 옵션 설정...
+  // useEffect(() => {
+  //   if (swiper === null) {
+  //     const newSwiper = new Swiper(".swiper-container", {
+  //       // Swiper 옵션 설정
+  //       slidesPerView: 2,
+  //       spaceBetween: 10,
+  //       loop: true,
+  //       loopedSlides: 1,
+  //       navigation: true,
+  //       // 다른 옵션 설정...
 
-        on: {
-          slideChange: function () {
-            const activeSlide = document.querySelector(".swiper-slide-active");
-            // 이전 활성 슬라이드에서 CSS 제거
-            const previousActiveSlide = document.querySelector(".active-slide");
-            if (previousActiveSlide) {
-              previousActiveSlide.classList.remove("active-slide");
-            }
-            // 현재 활성 슬라이드에 CSS 적용
-            activeSlide.classList.add("active-slide");
-          },
-        },
-      });
-      setSwiper(newSwiper);
-    }
-  }, [swiper]);
+  //       on: {
+  //         slideChange: function () {
+  //           const activeSlide = document.querySelector(".swiper-slide-active");
+  //           // 이전 활성 슬라이드에서 CSS 제거
+  //           const previousActiveSlide = document.querySelector(".active-slide");
+  //           if (previousActiveSlide) {
+  //             previousActiveSlide.classList.remove("active-slide");
+  //           }
+  //           // 현재 활성 슬라이드에 CSS 적용
+  //           activeSlide.classList.add("active-slide");
+  //         },
+  //       },
+  //     });
+  //     setSwiper(newSwiper);
+  //   }
+  // }, [swiper]);
 
   return (
     <div className="swiper-container">
