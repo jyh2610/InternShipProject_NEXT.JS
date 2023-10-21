2a7c98f5ddc5332961db95562c66a5bbfd4e23d0
uablejyh <jyh2610@uable.co.kr>
2023-10-21 22:13:33 +0900

    modify : next.config 서버, 디벨롭 환경설정 분리

--- front/next.config.js
+++ front/next.config.js
@@ -1,13 +1,21 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  server: {
-    host: "0.0.0.0",
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
+module.exports = module.exports = async (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      eslint: {
+        ignoreDuringBuilds: true,
+      },
+    };
+  }
+
+  return {
+    server: {
+      host: "0.0.0.0",
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  };
 };
-
-// next.config.js
-
-module.exports = nextConfig;
