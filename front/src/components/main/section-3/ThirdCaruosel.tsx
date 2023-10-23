64ccfcb8f642f490a15f330579447c2a6fe6548b
uablejyh <jyh2610@uable.co.kr>
2023-10-23 17:55:56 +0900

    wip

--- front/src/components/main/section-3/ThirdCaruosel.tsx
+++ front/src/components/main/section-3/ThirdCaruosel.tsx
@@ -3,40 +3,51 @@
 import "swiper/css";
 import "swiper/css/pagination";
 
-// import { Navigation } from "swiper/modules";
-// import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
-// import { img } from "@/constants/constants";
+import { img } from "@/constants/constants";
 
 import Slider from "./Slider";
 
-// import type { SwiperOptions } from "swiper/types";
-// eslint-disable-next-line import/order
-// import { img } from "@/constants/constants";
+import type { SwiperOptions } from "swiper/types";
+//eslint-disable-next-line import/order
 
 import "./style.css";
 
-// interface IdxProps {
-//   idx: number;
-//   prevRef: any;
-//   nextRef: any;
-// }
+interface IdxProps {
+  idx: number;
+  prevRef: any;
+  nextRef: any;
+}
 
-// function ThirdCarousel({ idx, prevRef, nextRef }: IdxProps) {
-function ThirdCarousel() {
-  // const swiperObj: SwiperOptions = {
-  //   loop: true,
-  //   slidesPerView: "auto",
-  //   spaceBetween: 20,
-  //   navigation: { prevEl: prevRef, nextEl: nextRef },
-  //   modules: [Navigation],
-  //   allowTouchMove: false,
-  // };
+function ThirdCarousel({ idx, prevRef, nextRef }: IdxProps) {
+  const swiperObj: SwiperOptions = {
+    loop: true,
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    navigation: { prevEl: prevRef, nextEl: nextRef },
+    modules: [Navigation],
+    allowTouchMove: false,
+  };
 
-  // const dummyImgData = [img, img, img, img, img, img, img, img];
+  const dummyImgData = [img, img, img, img, img, img, img, img];
   return (
     <>
-      <Slider />
+      {/* <Slider /> */}
+      <Swiper {...swiperObj} className="mySwiper ">
+        {dummyImgData.map((item, sliderIdx) => {
+          return idx === sliderIdx % 4 ? (
+            <SwiperSlide key={sliderIdx} className="aspect-w-16 aspect-h-9" style={{ width: "70%" }}>
+              <img className="w-full h-[500px]" src={item} alt="dummy" />
+            </SwiperSlide>
+          ) : (
+            <SwiperSlide className="aspect-w-16 aspect-h-9" style={{ width: "25%" }}>
+              <img className="w-full h-[500px] " src={item} alt="dummy" />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </>
   );
 }
