c3f0f5e26fea43ac49ba6b3c06e396dd4383850b
DevHyunJ <sonhyunji9819@gmail.com>
2023-10-23 16:13:10 +0900

    Fix: 메인 반응형 수정

--- front/src/components/main/section-5/MobileSlice.tsx
+++ front/src/components/main/section-5/MobileSlice.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { SetStateAction, useState } from "react";
 
 import { A11y, Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -8,7 +8,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import FadeProvider from "@/components/FadeProvider";
 import { data } from "@/constants/constants";
 
 const MobileSlice = () => {
@@ -18,25 +17,21 @@ const MobileSlice = () => {
     setImgIdx(swiper.realIndex);
   };
   return (
-    <div>
-      <FadeProvider>
-        <div className=" flex justify-center h-[40rem] ">
-          <Swiper style={{ margin: "0" }} className=" w-10/12" navigation={true} loop={true} modules={[Navigation, A11y]} onSlideChange={handleSlideChange}>
-            {data.map((item) => (
-              <SwiperSlide className="aspect-w-16 aspect-h-9" key={imgIdx}>
-                <img src={item.img} alt="이미지" />
-                <div className="text-[#fff]">
-                  <p className=" absolute top-[5rem] ml-[3rem] text-[3.75rem] font-bold">0{item.id + 1}</p>
-                  <div className="absolute ml-[3rem] bottom-[8rem] inset-0 flex flex-col justify-end transition-opacity">
-                    <p className="text-[1.5rem] mb-[2rem]" dangerouslySetInnerHTML={{ __html: item.title }} />
-                    <p dangerouslySetInnerHTML={{ __html: item.contentstext }} className=" text-[0.9rem]" />
-                  </div>
-                </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-      </FadeProvider>
+    <div className=" flex justify-center h-[40rem]">
+      <Swiper style={{ margin: "0" }} className=" w-10/12" navigation={true} loop={true} modules={[Navigation, A11y]} onSlideChange={handleSlideChange}>
+        {data.map((item) => (
+          <SwiperSlide className="aspect-w-16 aspect-h-9" key={imgIdx}>
+            <img src={item.img} alt="이미지" />
+            <div className="text-[#fff]">
+              <p className=" absolute top-[7rem] ml-[3rem] text-[3.75rem] font-bold">0{item.id + 1}</p>
+              <div className="absolute ml-[3rem] bottom-[23rem] inset-0 flex flex-col justify-end transition-opacity">
+                <p className="text-[1.5rem] mb-[2rem]" dangerouslySetInnerHTML={{ __html: item.title }} />
+                <p dangerouslySetInnerHTML={{ __html: item.contentstext }} className="text-[0.9rem]" />
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
