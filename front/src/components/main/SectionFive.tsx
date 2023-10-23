3c24cfa1ff710599b89cd60da8947cf266fdf842
DevHyunJ <sonhyunji9819@gmail.com>
2023-10-23 10:52:38 +0900

    Fix: ssr 수정

--- front/src/components/main/SectionFive.tsx
+++ front/src/components/main/SectionFive.tsx
@@ -1,11 +1,7 @@
-"use client";
 import React from "react";
-
-// import { useMediaQuery } from "react-responsive";
 
 import "../../components/main/section-5/style.css";
 import HoverImage from "./section-5/HoverImage";
-import { Mobile } from "./section-5/Mobile";
 import MobileSlice from "./section-5/MobileSlice";
 
 function SectionFive() {
