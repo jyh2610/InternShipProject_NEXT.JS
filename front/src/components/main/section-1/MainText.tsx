579ff8151936c0bdeccb2fdaff0516c810b63999
kim-teak-yong <144188911+tackyong@users.noreply.github.com>
2023-10-23 14:01:44 +0900

    10월 23일

--- front/src/components/main/section-1/MainText.tsx
+++ front/src/components/main/section-1/MainText.tsx
@@ -17,7 +17,8 @@ function MainText() {
       >
         <div className="tit">
           <div className=" text-4xl md:text-6xl font-light leading-[3rem] md:leading-[4.3rem]">
-            ㅔ <br />
+            드래그 앤 드롭으로
+            <br />
             손쉬운 3D 가상인테리어 디자인
           </div>
           <div className="pt-3.5 pb-14 text-lg md:text-xl">공간은 이야기를 품고 있습니다. 우리는 그이야기를 아름답게 만듭니다</div>
