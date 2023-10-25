cd4b61d0b99d2228e215674b32b14c488ae113f1
uablejyh <jyh2610@uable.co.kr>
2023-10-25 10:18:39 +0900

    fix: nav  햄버거메뉴 css 수정

--- front/next.config.js
+++ front/next.config.js
@@ -1,6 +1,34 @@
+const withPlugins = require("next-compose-plugins");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: true,
+  openAnalyzer: true,
+});
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 /** @type {import('next').NextConfig} */
 
+// const nextConfig = async (phase) => {
+//   if (phase === PHASE_DEVELOPMENT_SERVER) {
+//     return {
+//       eslint: {
+//         ignoreDuringBuilds: true,
+//       },
+//     };
+//   }
+
+//   return {
+//     server: {
+//       host: "0.0.0.0",
+//     },
+//     eslint: {
+//       ignoreDuringBuilds: true,
+//     },
+//   };
+// };
+// module.exports = withPlugins([
+//   [withBundleAnalyzer],
+//   nextConfig,
+//   // your other plugins here
+// ]);
 module.exports = async (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
