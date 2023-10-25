a979e8d5d988b062db33e9eb33240edc77d4de32
uablejyh <jyh2610@uable.co.kr>
2023-10-25 15:11:03 +0900

    fix: log wprj

--- front/src/app/api/auth/[...nextauth]/route.ts
+++ front/src/app/api/auth/[...nextauth]/route.ts
@@ -31,7 +31,6 @@ const handler = NextAuth({
       if (account) {
         token.accessToken = account.access_token;
         const socialaccesstoken = token?.accessToken;
-        console.log("----------------account------------------------------------------------", account, "account");
 
         const url = `/sign/${account.provider}login`;
         const res = await api.post({
@@ -49,11 +48,7 @@ const handler = NextAuth({
     },
     async session({ session, token }: any) {
       session.accessToken = token.customData;
-      console.log(
-        "----------------session------------------------------------------------",
-        session,
-        "----------------session------------------------------------------------",
-      );
+
       return session;
     },
   },
