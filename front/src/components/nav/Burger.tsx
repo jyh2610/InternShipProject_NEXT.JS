0e04ae177e4cf3d4791f50489ec4f0a4779f6e5e
uablejyh <jyh2610@uable.co.kr>
2023-10-26 10:13:32 +0900

    feat: 햄버거 메뉴 레이아웃 flex 적용

--- front/src/components/nav/Burger.tsx
+++ front/src/components/nav/Burger.tsx
@@ -13,37 +13,34 @@ export default function Burger({ open, setOpen }: { open: boolean; setOpen: Func
     top: "0",
     left: "0",
     background: "white",
-    display: "flex",
-    flexDirection: "column",
     gap: "2rem",
-    justifyContent: "start",
-    alignItems: "center",
     marginTop: "60px",
   } as React.CSSProperties;
 
   return (
     <ul style={menuStyle}>
-      <li>
-        <div onClick={() => router.push("/signin")}>로그인</div>
-      </li>
-      <li>
-        <div onClick={() => router.push("/")}>홈</div>
-      </li>
-      <li>
-        <div>제품</div>
-        <ul style={{ display: "none" }}>
-          <li>디자인&렌더</li>
-          <li>애니매이션</li>
-          <li>브랜드갤러리</li>
-        </ul>
-      </li>
-      <li>
-        <div>솔루션</div>
-      </li>
-      <li>
-        {" "}
-        <div onClick={() => router.push("/download")}>다운로드</div>
-      </li>
+      <div className="h-2/3 flex flex-col items-center justify-between pt-[60px]">
+        <li>
+          <div onClick={() => router.push("/signin")}>로그인</div>
+        </li>
+        <li>
+          <div onClick={() => router.push("/")}>홈</div>
+        </li>
+        <li className="h-24 flex flex-col justify-between items-center">
+          <div>제품</div>
+          <ul>
+            <li>디자인&렌더</li>
+            <li>애니매이션</li>
+            <li>브랜드갤러리</li>
+          </ul>
+        </li>
+        <li>
+          <div>솔루션</div>
+        </li>
+        <li>
+          <div onClick={() => router.push("/download")}>다운로드</div>
+        </li>
+      </div>
     </ul>
   );
 }
