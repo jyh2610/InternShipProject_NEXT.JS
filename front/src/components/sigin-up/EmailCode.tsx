5497b630455016b830446a7bfd580221243ac51a
DevHyunJ <sonhyunji9819@gmail.com>
2023-10-27 11:18:58 +0900

    Fix:이메일 인증 수정

--- front/src/components/sigin-up/EmailCode.tsx
+++ front/src/components/sigin-up/EmailCode.tsx
@@ -9,29 +9,41 @@ import Timer from "./Timer";
 function EmailCode({ email, setIsActive, isActive }: { isActive: boolean; email: string; setIsActive: React.Dispatch<React.SetStateAction<boolean>> }) {
   const [code, setCode] = useState("");
   const [seconds, setSeconds] = useState(180);
+  const [resSuccess, setResSuccess] = useState("");
   const api = new baseApi();
   const startTimer = () => {
     setSeconds(180);
-    api.post({
-      url: "/validate/verifycode",
-      body: {
-        email: email,
-        code: code,
-      },
-    });
+    api
+      .post({
+        url: "/validate/verifycode",
+        body: {
+          email: email,
+          code: code,
+        },
+      })
+      .then((res) => {
+        console.log(res.success);
+        setResSuccess(res.success);
+      });
   };
 
   return (
-    <div className="flex">
-      <Input
-        style={{ width: "300px" }}
-        value={code}
-        onChange={(e) => setCode(e.target.value)}
-        name="code"
-        suffix={<Timer setSeconds={setSeconds} setIsActive={setIsActive} isActive={isActive} seconds={seconds} />}
-      />
-      <Button onClick={startTimer}>인증하기</Button>
-    </div>
+    <>
+      {resSuccess ? (
+        <p className="text-[#26AF66] font-bold">인증이 완료되었습니다.</p>
+      ) : (
+        <div className="flex">
+          <Input
+            style={{ width: "300px" }}
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+            name="code"
+            suffix={<Timer setSeconds={setSeconds} setIsActive={setIsActive} isActive={isActive} seconds={seconds} />}
+          />
+          <Button onClick={startTimer}>인증하기</Button>
+        </div>
+      )}
+    </>
   );
 }
 
