8c680ab344599a5dd45ef95aa073ded263b10259
Uable-hrd <hrd1@uable.co.kr>
2023-10-27 15:17:35 +0900

    Feat: signupcheck api 추가(닉네임으로 요청=> 이름, 가입일 반환)

--- back/src/controllers/validateController.js
+++ back/src/controllers/validateController.js
@@ -20,6 +20,14 @@ const hasNickname = catchAsync(async(req, res) => {
     return res.status(200).json(await validateService.isDuplicateNickname(nickname));
 });
 
+//회원가입 완료한 유저의 이름, 가입시간
+const signUpCheck = catchAsync(async(req, res) => {
+    const {nickname} = req.body;
+    if (!nickname) detectError("KEY_ERROR", 400);
+
+    return res.status(200).json(await validateService.signUpCheck(nickname));
+});
+
 // 인증코드 이메일 전송
 const sendEmail = catchAsync(async(req, res) => {
     return res.status(200).json(await validateService.emailValidation(req.body.email));
@@ -45,6 +53,8 @@ const reissuanceAeccessToken = catchAsync(async(req, res) => {
 module.exports ={
     hasId,
     hasNickname,
+
+    signUpCheck,
 
     sendEmail,
     verifyCode,
