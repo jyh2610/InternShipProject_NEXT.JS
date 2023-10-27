a3ccd0abc774eb8ca891b2e66bb7cab2bed053a4
taekyong <kty@uable.co.kr>
2023-10-27 18:12:39 +0900

    모바일 상단 작업

--- front/src/components/nav/Burger.tsx
+++ front/src/components/nav/Burger.tsx
@@ -22,45 +22,53 @@ export default function Burger() {
   useEffect(() => {
     document.body.style.overflow = "hidden";
     return () => {
-      document.body.style.overflow = "unset";
+      document.body.style.overflow = "visible";
     };
   }, []);
 
   return (
-    <div style={menuStyle}>
-      <ul className=" flex flex-col items-center justify-between py-[60px] gap-5">
+    <div className="" style={menuStyle}>
+      <ul className=" flex flex-col items-center justify-between pt-[30px]">
         <li className="w-full">
-          <Space className="w-full flex flex-col">
-            <Button style={{ width: "100vw", height: "3rem", backgroundColor: "green" }} onClick={() => setIsDropDown((prev) => !prev)}>
-              제품
-            </Button>
-            {dropdown && (
-              <ul className="w-screen flex flex-col justify-between gap-3 pt-5">
-                <Button onClick={() => router.push("/design")} style={{ height: "3rem", backgroundColor: "green" }} type="text" className="w-full">
-                  디자인&렌더
-                </Button>
-                <Button onClick={() => router.push("/animation")} style={{ height: "3rem", backgroundColor: "green" }} type="text" className="w-full">
-                  애니매이션
-                </Button>
-                <Button onClick={() => router.push("/brand/gallery")} style={{ height: "3rem", backgroundColor: "green" }} type="text" className="w-full">
-                  브랜드갤러리
-                </Button>
-              </ul>
-            )}
-          </Space>
+          <Button
+            className="w-full"
+            type="text"
+            style={{ borderRadius: "0", borderBottom: "1px solid #E2E2E2" }}
+            onClick={() => setIsDropDown((prev) => !prev)}
+          >
+            제품
+          </Button>
+          {dropdown && (
+            <ul className="w-full flex flex-col justify-between ">
+              <Button onClick={() => router.push("/design")} style={{ height: "2rem" }} type="text" className="w-full">
+                디자인&렌더
+              </Button>
+              <Button onClick={() => router.push("/animation")} style={{ height: "2rem" }} type="text" className="w-full">
+                애니매이션
+              </Button>
+              <Button onClick={() => router.push("/brand/gallery")} style={{ height: "2rem" }} type="text" className="w-full">
+                브랜드갤러리
+              </Button>
+            </ul>
+          )}
         </li>
         <li className="w-full">
-          <Button style={{ height: "3.5rem", backgroundColor: "green" }} type="text" className="w-full" onClick={() => router.push("/solution")}>
+          <Button style={{ borderRadius: "0", borderBottom: "1px solid #E2E2E2" }} type="text" className="w-full" onClick={() => router.push("/solution")}>
             솔루션
           </Button>
         </li>
         <li className="w-full">
-          <Button style={{ height: "3.5rem", backgroundColor: "green" }} type="text" className="w-full" onClick={() => router.push("/download")}>
+          <Button style={{ borderRadius: "0", borderBottom: "1px solid #E2E2E2" }} type="text" className="w-full" onClick={() => router.push("/download")}>
             다운로드
           </Button>
         </li>
         <li className="w-full">
-          <Button type="text" style={{ height: "3rem", backgroundColor: "green" }} className="w-full bg-ugreen" onClick={() => router.push("/signin")}>
+          <Button
+            type="text"
+            style={{ borderRadius: "0", borderBottom: "1px solid #E2E2E2" }}
+            className="w-full bg-ugreen"
+            onClick={() => router.push("/signin")}
+          >
             로그인
           </Button>
         </li>
