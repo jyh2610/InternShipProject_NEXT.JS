6b69806201dba49b0e9671351863d0a49658483f
uablejyh <jyh2610@uable.co.kr>
2023-10-27 17:53:19 +0900

    style: 아이콘 색변경

--- front/src/components/nav/Hamberger.tsx
+++ front/src/components/nav/Hamberger.tsx
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import { CloseOutlined, MenuOutlined } from "@ant-design/icons";
 import Burger from "./Burger";
+import { usePathname } from "next/navigation";
 
-function Hamberger({ open, setOpen }: { open: boolean; setOpen: Function }) {
+function Hamberger({ open, setOpen, scrollY }: { open: boolean; setOpen: Function; scrollY: number }) {
+  const router = usePathname();
+  const isTop = router !== "/" ? "black" : scrollY === 0 ? "white" : "black";
   return (
     <>
-      {!open ? <MenuOutlined onClick={() => setOpen((prev: boolean) => !prev)} /> : <CloseOutlined onClick={() => setOpen(false)} />}
+      {!open ? (
+        <MenuOutlined style={{ color: isTop }} onClick={() => setOpen(true)} />
+      ) : (
+        <CloseOutlined style={{ color: isTop }} onClick={() => setOpen(false)} />
+      )}
       {open && <Burger />}
     </>
   );
