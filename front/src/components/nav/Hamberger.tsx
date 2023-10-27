594a90a2351b6973569e6da14d2b41d3fd49d2e5
uablejyh <jyh2610@uable.co.kr>
2023-10-27 17:32:49 +0900

    style: 햄버거메뉴 antd 아이콘 변경

--- front/src/components/nav/Hamberger.tsx
+++ front/src/components/nav/Hamberger.tsx
@@ -1,29 +1,12 @@
 import React, { useState } from "react";
 
-import { MenuOutlined } from "@ant-design/icons";
+import { CloseOutlined, MenuOutlined } from "@ant-design/icons";
 import Burger from "./Burger";
 
 function Hamberger({ open, setOpen }: { open: boolean; setOpen: Function }) {
   return (
     <>
-      {!open ? (
-        <MenuOutlined onClick={() => setOpen((prev: boolean) => !prev)} />
-      ) : (
-        <div onClick={() => setOpen(false)}>
-          <svg
-            className="h-8 w-8 text-gray-600"
-            viewBox="0 0 26 26"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <line x1="18" y1="6" x2="6" y2="18" />
-            <line x1="6" y1="6" x2="18" y2="18" />
-          </svg>
-        </div>
-      )}
+      {!open ? <MenuOutlined onClick={() => setOpen((prev: boolean) => !prev)} /> : <CloseOutlined onClick={() => setOpen(false)} />}
       {open && <Burger />}
     </>
   );
