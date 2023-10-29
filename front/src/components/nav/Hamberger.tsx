e325abea127dfbf5eb2671c6240e181adc5f20c4
uablejyh <jyh2610@uable.co.kr>
2023-10-29 20:39:36 +0900

    feat: lazy로딩 적용 및 함수 모듈화

--- front/src/components/nav/Hamberger.tsx
+++ front/src/components/nav/Hamberger.tsx
@@ -8,14 +8,14 @@ function Hamberger({ open, setOpen, scrollY }: { open: boolean; setOpen: Functio
   const router = usePathname();
   const isTop = router !== "/" ? "black" : scrollY === 0 ? "white" : "black";
   return (
-    <>
+    <div className="mo">
       {!open ? (
         <MenuOutlined style={{ color: isTop }} onClick={() => setOpen(true)} />
       ) : (
         <CloseOutlined style={{ color: "black" }} onClick={() => setOpen(false)} />
       )}
       {open && <Burger />}
-    </>
+    </div>
   );
 }
 
