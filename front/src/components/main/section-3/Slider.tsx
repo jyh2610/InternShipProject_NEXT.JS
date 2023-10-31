e9456ec8825aab18d1b1dcda21a7cff598428904
uablejyh <jyh2610@uable.co.kr>
2023-10-31 17:07:04 +0900

    style: 데이터 객체화

--- front/src/components/main/section-3/Slider.tsx
+++ front/src/components/main/section-3/Slider.tsx
@@ -4,12 +4,11 @@ import React from "react";
 import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import { img } from "@/constants/constants";
+import { thirdSlideData } from "@/constants/constants";
 
 import type { SwiperOptions } from "swiper/types";
 
 function Slider() {
-  const dummyImgData = [img, img, img, img];
   const swiperObj: SwiperOptions = {
     loop: true,
     slidesPerView: 1,
@@ -19,10 +18,10 @@ function Slider() {
 
   return (
     <Swiper navigation={true} {...swiperObj} className="mySwiper max-w-top">
-      {dummyImgData.map((item, idx) => {
+      {thirdSlideData.map((item, idx) => {
         return (
           <SwiperSlide key={idx} className="max-w-top">
-            <img key={idx} src={item} alt="img" className="w-full h-[500px]" />
+            <img key={idx} src={item.img} alt="img" className="w-full h-[500px]" />
           </SwiperSlide>
         );
       })}
