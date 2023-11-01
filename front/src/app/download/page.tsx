b0e33f5971177bccc2dad9c9ab0d1fdbc7c245a2
taekyong <kty@uable.co.kr>
2023-11-01 13:48:18 +0900

    다운로드페이지 애니메이션 복구

--- front/src/app/download/page.tsx
+++ front/src/app/download/page.tsx
@@ -3,15 +3,16 @@ import React from "react";
 import "./style.css";
 import { CloudDownloadOutlined } from "@ant-design/icons";
 import Link from "next/link";
+import FadeProvider from "@/components/FadeProvider";
 
 function Download() {
   return (
-    <>
+    <FadeProvider>
       <div className="wrap">
         <div className="top_section">
           <div className="mainwidth mx-auto h-[100%] relative">
             <div className="page-text">
-              <div className="text-layout">
+              <div className="text-layout" data-aos="fade-up" data-aos-delay="80">
                 <h4 className="page-tit">
                   인테리어 디자이너를 위한
                   <br />
@@ -40,13 +41,13 @@ function Download() {
         <div className="container_section">
           <div className="guide">
             <div className="mainwidth mx-auto">
-              <div className="section-text-wrap">
+              <div className="section-text-wrap" data-aos="fade-up" data-aos-offset="100" data-aos-delay="100">
                 <h4 className="section-tit">인테리어는 당연히 쉽고 편해야죠</h4>
                 <p className="section-text">아키플 스튜디오를 사용하는 모두를 위해</p>
               </div>
               <div className="item-list">
                 <ul className="item-wrap">
-                  <li className="item">
+                  <li className="item" data-aos="fade-up" data-aos-offset="150" data-aos-delay="100">
                     <div className="item-tit">
                       <span className="badge marketing">마케팅</span>
                       <h3>고객의 다양한 니즈에 맞게 원하는 디자인을 빠르게 제공</h3>
@@ -55,7 +56,7 @@ function Download() {
                       <p>고객의 니즈를 파악하여, 사실적인 텍스쳐로 고객의 생각을 시각적으로 볼 수 있도록 제공 합니다.</p>
                     </div>
                   </li>
-                  <li className="item">
+                  <li className="item" data-aos="fade-up" data-aos-offset="150" data-aos-delay="200">
                     <div className="item-tit">
                       <span className="badge partner">파트너</span>
                       <h3>당신만의 소중한 제품을 가상현실 환경에서도 제공</h3>
@@ -64,7 +65,7 @@ function Download() {
                       <p>기존 형식의 온라인 판매가 아닌 가상현실에서 유저가 사용해보며 실 구매회전율 상승.</p>
                     </div>
                   </li>
-                  <li className="item">
+                  <li className="item" data-aos="fade-up" data-aos-offset="150" data-aos-delay="300">
                     <div className="item-tit">
                       <span className="badge user">유저</span>
                       <h3>나만의 공간디자인을 많은 유저들에게 공유하여, 나만의 포트폴리오</h3>
@@ -80,13 +81,13 @@ function Download() {
         </div>
         <div className="bottom_section">
           <div className="mainwidth mx-auto">
-            <div className="section-text-wrap pt-[10rem] pb-[3rem]">
+            <div className="section-text-wrap pt-[10rem] pb-[3rem]" data-aos="fade-up" data-aos-offset="200" data-aos-delay="100">
               <h4 className="section-tit">빠르고 간단하게 설치해야죠.</h4>
               <p className="section-text">설치방법 안내</p>
             </div>
           </div>
           <div className="install-list">
-            <div className="install-01-wrap">
+            <div className="install-01-wrap" data-aos="fade-up" data-aos-offset="230" data-aos-delay="230">
               <div className="install-text">
                 <p>
                   <span className="install-num">01</span>
@@ -129,7 +130,7 @@ function Download() {
                     <div className="img-wrap">
                       <img
                         src="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaCpOo1I10Tgoh8eSUG0eUOWdQzqaCZnqe4BGDuCqKx7iHrG8ZPVsgS7QN-VXCLCYzXNpuz6O9ibSwf-HnMIw-S97ObJ=w1920-h888"
-                        alt=""
+                        alt="런처 설치방법 첫 번째"
                       />
                     </div>
                     <div className="item-txt">
@@ -140,7 +141,7 @@ function Download() {
                     <div className="img-wrap">
                       <img
                         src="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaCmxRHdX-2YW2b4smnP3xQ5IaDzv5CDWKNuRxFMqBs9K7U7trp6mFgCAjXOtiKOxBjrJI80_atqaJ2cGyh36srBG0LBKg=w1920-h888"
-                        alt=""
+                        alt="런처 설치방법 두 번째"
                       />
                     </div>
                     <div className="item-txt">
@@ -151,7 +152,7 @@ function Download() {
                     <div className="img-wrap">
                       <img
                         src="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaCYiw_8_ANPaInQHILOoJUoUyrIoNZBN3O-Qe45urDuyrP9Ms9Sf4EKbY8bXHjEmMxuiPrzXjq7BjlXhoKG_s5gXhCw=w1920-h888"
-                        alt=""
+                        alt="런처 설치방법 세 번째"
                       />
                     </div>
                     <div className="item-txt">
@@ -162,11 +163,11 @@ function Download() {
                     <div className="img-wrap">
                       <img
                         src="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaCIkyD2ak7PRVID3meEJusdHdInQV8l0NdNGvM4H0UAFYhGhLSpxGg-xkuNoRCXtPRQL2KTtz3Ipzo3Uk_zgID4eJFuCw=w1920-h888"
-                        alt=""
+                        alt="런처 설치방법 네 번째"
                       />
                     </div>
                     <div className="item-txt">
-                      <p>04) 유에이블 홈페이지에서 회원가입합니다.</p>
+                      <p>04) 런처 다운로드가 완료 되었습니다.</p>
                     </div>
                   </li>
                 </ul>
@@ -186,23 +187,34 @@ function Download() {
                   <li className="install-item">
                     <div className="img-wrap">
                       <img
-                        src="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaBfewyceNZd8A8pSPVmLQ8vteXfwkK3olqHiTJ1wLlRl8pgJ7r-oShEhLWpMKLoJXHzO1sKKZO_61AUA89f-KJnmawH1A=w1920-h888"
-                        alt=""
+                        src="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaAKrUvbQlLUf6hCCtmsEaqCKaq1YvML8ijCKi-lIPiW9TOsT6hgP5EttL8PmFPmrgLBD--uEd0d9JWAtdDt7lcRzzzSnQ=w1920-h888"
+                        alt="아키플 스튜디오 설치 첫 번째"
                       />
                     </div>
                     <div className="item-txt">
-                      <p>Archple Studio에 들어가 Intall을 클릭 합니다.</p>
+                      <p>01) Archple Studio에 들어가 Intall을 클릭 합니다.</p>
                     </div>
                   </li>
                   <li className="install-item">
                     <div className="img-wrap">
                       <img
-                        src="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaBfewyceNZd8A8pSPVmLQ8vteXfwkK3olqHiTJ1wLlRl8pgJ7r-oShEhLWpMKLoJXHzO1sKKZO_61AUA89f-KJnmawH1A=w1920-h888"
-                        alt=""
+                        src="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaB8bveSohAvToJGS2xpKaNPpadva2Q7wGzfFzVRp504XDwdtGDkpFKqJW2ckoveq2WUXIWn-a4DBG-pTbL9U7ocbB1SdQ=w1920-h888"
+                        alt="아키플 스튜디오 설치 두 번째"
                       />
                     </div>
                     <div className="item-txt">
-                      <p>다운로드한 Archple Studio를 실행합니다.</p>
+                      <p>02) 설치 경로를 지정하고 설치를 진행 합니다.</p>
+                    </div>
+                  </li>
+                  <li className="install-item">
+                    <div className="img-wrap">
+                      <img
+                        src="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaACw-s5Nycf8tBfk0eM4vvC9WiZlmEyJH6rrY9KecS-t0urKHfhfwdcsIyQ1SVmy5050_gC816GO05I9SjvaDLKtkxi=w1920-h888"
+                        alt="아키플 스튜디오 설치 세 번째"
+                      />
+                    </div>
+                    <div className="item-txt">
+                      <p>03) Archple Studio 2.0 설치가 완료 되었습니다.</p>
                     </div>
                   </li>
                 </ul>
@@ -211,7 +223,7 @@ function Download() {
           </div>
         </div>
       </div>
-    </>
+    </FadeProvider>
   );
 }
 
