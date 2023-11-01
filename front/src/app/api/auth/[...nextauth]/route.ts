2afd5537b8377c03b9a5b156ae76d46397bbccfc
Ubuntu <ubuntu@ip-172-31-37-68.ap-northeast-2.compute.internal>
2023-11-01 18:11:54 +0900

    디버깅 추가

--- front/src/app/api/auth/[...nextauth]/route.ts
+++ front/src/app/api/auth/[...nextauth]/route.ts
@@ -26,12 +26,13 @@ const authOption = {
     },
     async jwt({ token, account }: any) {
       token.token = account?.access_token;
+      console.log(account, 1 )
       if (account) {
         token.token = account?.access_token;
-        const url = `/sign/${account?.provider}login`;
-        console.log(url);
+        const url =`/sign/${account?.provider}login`
+        console.log(url,2);
 
-        console.log(account);
+        console.log(account,3);
 
         const res: CustomSession = await api.post({
           url,
