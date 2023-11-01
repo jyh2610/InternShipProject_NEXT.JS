17570851d68bc417afd86471da87cf220d7770d2
DevHyunJ <sonhyunji9819@gmail.com>
2023-11-01 17:29:55 +0900

    Fix:인증번호가 다를 때, 인증하기 버튼 클릭 시 3분 갱신되는 문제 해결

--- front/src/components/sigin-up/EmailCode.tsx
+++ front/src/components/sigin-up/EmailCode.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Button, Input } from "antd";
 
@@ -10,8 +10,14 @@ function EmailCode({ email, setIsActive, isActive }: { isActive: boolean; email:
   const [code, setCode] = useState("");
   const [seconds, setSeconds] = useState(180);
   const [resSuccess, setResSuccess] = useState("");
+  const [timestart, setTimestart] = useState(false);
   const api = new baseApi();
   const startTimer = () => {
+    setTimestart(true);
+    // 이미 타이머가 시작 중이라면 무시
+    if (timestart) {
+      return;
+    }
     setSeconds(180);
     api
       .post({
@@ -26,6 +32,26 @@ function EmailCode({ email, setIsActive, isActive }: { isActive: boolean; email:
         setResSuccess(res.success);
       });
   };
+
+  // 타이머 감시
+  useEffect(() => {
+    startTimer();
+    let timerInterval: any;
+
+    if (seconds > 0) {
+      timerInterval = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds - 1);
+      }, 1000);
+    } else {
+      clearInterval(timerInterval);
+      setTimestart(false); // 타이머 종료 상태로 설정
+    }
+
+    // 컴포넌트가 언마운트될 때 타이머 정리
+    return () => {
+      clearInterval(timerInterval);
+    };
+  }, [seconds]);
   return (
     <>
       {resSuccess ? (
