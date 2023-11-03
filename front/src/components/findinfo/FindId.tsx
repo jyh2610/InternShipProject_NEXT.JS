39817fdb4807ba88f39aa9e261976e8fba94b368
tackyong <144188911+tackyong@users.noreply.github.com>
2023-11-03 21:07:36 +0900

    아이디찾기 폼 완료

--- front/src/components/findinfo/FindId.tsx
+++ front/src/components/findinfo/FindId.tsx
@@ -13,8 +13,8 @@ function Findid() {
     <Space className="w-full">
       <div>
         <div className="find-text-box">
-          <h4 className="find-tit">등록 된 이메일로 찾기</h4>
-          <p>가입 시 입력한 이메일을 통해 인증 후 아이디를 확인 해주세요. </p>
+          <p className="find-tit">등록 된 이메일로 찾기</p>
+          <p className="find-text">가입 시 입력한 이메일을 통해 인증 후 아이디를 확인 해주세요. </p>
         </div>
         <div className="find-from-wrap">
           <form className="name findinput">
@@ -25,7 +25,7 @@ function Findid() {
             <label htmlFor="이메일">이메일</label>
             <div className="emall-select">
               <input type="emall" id="emall" name="이메일" />
-              <p className="font-bold text-[1rem]">@</p>
+              <p className="font-bold text-[1rem] text-[#bfbfbf]">@</p>
               <ConfigProvider
                 theme={{
                   components: {
@@ -52,7 +52,21 @@ function Findid() {
               </ConfigProvider>
             </div>
           </form>
+          <form className="certified findinput">
+            <label htmlFor="인증번호">인증번호</label>
+            <div className="w-full">
+              <div className="ce-code">
+                <input type="number" id="num" name="인증번호" />
+                <button className="code-cp-btn" type="submit">
+                  인증하기
+                </button>
+              </div>
+              <span className="alert-text">인증번호를 넣어주세요.</span>
+              <span className="completion alert-text">인증되셨습니다.</span>
+            </div>
+          </form>
         </div>
+        <button className="completion-btn">확인</button>
       </div>
     </Space>
   );
