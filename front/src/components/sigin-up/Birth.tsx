1479e5d8143533eaf9fe85af3cc9fa5a6ab0cbab
DevHyunJ <sonhyunji9819@gmail.com>
2023-11-03 18:09:03 +0900

    Fix: 달력 한국어 버전 변경

--- front/src/components/sigin-up/Birth.tsx
+++ front/src/components/sigin-up/Birth.tsx
@@ -1,17 +1,29 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
 
 import { ConfigProvider, DatePicker, Form } from "antd";
 import moment from "moment";
 import "moment/locale/ko";
 import locale from "antd/lib/locale/ko_KR";
-//moment.locale("ko");
+import dayjs from "dayjs";
+
+import "dayjs/locale/ko";
+
+import updateLocale from "dayjs/plugin/updateLocale";
+
+dayjs.extend(updateLocale);
+dayjs.updateLocale("ko", {
+  weekStart: 0,
+});
+
 export interface ValidProps {
   validateSelect: (_: unknown, value: string | number) => Promise<void>;
   ko_KR: string;
 }
+
 function Birth({ validateSelect }: ValidProps) {
   // 영어 로케일 비활성화
-  //moment.updateLocale("en", null);
+
   moment.locale("ko");
   return (
     <ConfigProvider locale={locale}>
--- front/src/components/sigin-up/SiginupForm.tsx
+++ front/src/components/sigin-up/SiginupForm.tsx
@@ -19,6 +19,7 @@ import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { setUserName } from "@/redux/slicer/authSlice";
 import EmailNew from "./EmailNew";
 import { useEffect, useRef, useState } from "react";
+import NewBirth from "./NewBirth";
 
 const validateMessages = {
   required: "${label} is required!",
@@ -139,6 +140,7 @@ const SiginupForm = () => {
             />
             <TestPassword />
             <Birth validateSelect={validateSelect} ko_KR={""} />
+            {/* <NewBirth /> */}
             <Sex validateSelect={validateSelect} ko_KR={""} />
             <SiginupBtn validateForm={validateForm} />
           </Form>
