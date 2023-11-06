16ff48e1b36e0a01c17f4986295984ef467ae5e4
tackyong <144188911+tackyong@users.noreply.github.com>
2023-11-06 23:28:41 +0900

    진행 순서 탭으로 변경 및 준비중인 페이지 디자인 변경

--- front/src/components/main/section-5/TabImage.tsx
+++ front/src/components/main/section-5/TabImage.tsx
@@ -1,21 +1,16 @@
 "use client";
 import React, { useState } from "react";
-import type { RadioChangeEvent } from "antd";
-import { ConfigProvider, Radio, Space, Tabs } from "antd";
-import { slideData, tab_img } from "@/constants/constants";
+import { ConfigProvider, Tabs } from "antd";
+import { tab_img } from "@/constants/constants";
 import Image from "next/image";
 
 type TabPosition = "top";
 
 const TabImage = () => {
-  const [tabPosition, setTabPosition] = useState<TabPosition>("top");
-
-  const changeTabPosition = (e: RadioChangeEvent) => {
-    setTabPosition(e.target.value);
-  };
+  const [tabPosition] = useState<TabPosition>("top");
 
   return (
-    <div className="mainwidth mx-auto">
+    <div className="pl-[12rem]">
       <ConfigProvider
         theme={{
           components: {
@@ -32,17 +27,17 @@ const TabImage = () => {
           items={tab_img.map((item, i) => {
             const id = String(i + 1);
             return {
-              label: <div style={{ fontSize: "1.2rem", color: "#000" }}>0{item.id + 1}</div>,
+              label: <div style={{ fontSize: "1.25rem", color: "#000" }}>0{item.id + 1}</div>,
               key: id,
               children: (
                 <div className="w-full">
                   <div key={id} className="tab_contant flex gap-[2rem]">
-                    <div className="tab_text" style={{ width: "100%", maxWidth: "640px" }} key={id}>
-                      <p className="" dangerouslySetInnerHTML={{ __html: item.title }} />
-                      <p dangerouslySetInnerHTML={{ __html: item.contentstext }} className=" text-[1.2rem] font-medium" />
+                    <div className="tab_text" key={id}>
+                      <p className="tab-tit" dangerouslySetInnerHTML={{ __html: item.title }} />
+                      <p dangerouslySetInnerHTML={{ __html: item.contentstext }} />
                     </div>
-                    <div className="tab_img">
-                      <Image width={1600} height={640} src={item.img} alt={item.title} />
+                    <div className="tab_img shadow-2xl">
+                      <Image width={1600} height={200} src={item.img} alt={item.title} />
                     </div>
                   </div>
                 </div>
